b4a2b2594b965cdb822ae8b56733a7052ad17548
SohamJ513 <sohamjathar004@gmail.com>
2025-12-19 19:20:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,6 @@ import {
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  const firebaseConfig = {
   apiKey: "YOUR_API_KEY_HERE",
   authDomain: "YOUR_AUTH_DOMAIN_HERE",
   databaseURL: "YOUR_DATABASE_URL_HERE",
@@ -22,7 +21,6 @@ const firebaseConfig = {
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
   appId: "YOUR_APP_ID_HERE",
   measurementId: "YOUR_MEASUREMENT_ID_HERE"
-};
 };
 
 // Initialize Firebase
